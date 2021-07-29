f9beb986de58987beadc149eebdac277f25803d4
CodeReviewer14 <88056155+CodeReviewer14@users.noreply.github.com>
2021-07-29 12:38:46 +0300

    fix glow command

--- src/extension.js
+++ src/extension.js
@@ -29,11 +29,11 @@ function activate(context) {
 		}
 
 	}
-
+	
 	const config = vscode.workspace.getConfiguration("cyber77");
 
 	let disableGlow = config && config.disableGlow ? !!config.disableGlow : false;
-
+	
 	let brightness = parseFloat(config.brightness) > 1 ? 1 : parseFloat(config.brightness);
 	brightness = brightness < 0 ? 0 : brightness;
 	brightness = isNaN(brightness) ? 0.45 : brightness;
@@ -55,36 +55,21 @@ function activate(context) {
           : "/electron-browser/workbench/workbench.html");
 
       const templateFile =
-        base +
-        (isWin
-          ? "\\electron-browser\\workbench\\neondreams.js"
-          : "/electron-browser/workbench/neondreams.js");
+          base +
+          (isWin
+            ? "\\electron-browser\\workbench\\neondreams.js"
+            : "/electron-browser/workbench/neondreams.js");
       try {
         // const version = context.globalState.get(`${context.extensionName}.version`);
 
         // generate production theme JS
-        const chromeStyles = fs.readFileSync(
-          __dirname + "/css/editor_chrome.css",
-          "utf-8"
-        );
-        const jsTemplate = fs.readFileSync(
-          __dirname + "/js/theme_template.js",
-          "utf-8"
-        );
-        const themeWithGlow = jsTemplate.replace(
-          /\[DISABLE_GLOW\]/g,
-          disableGlow
-        );
-        const themeWithChrome = themeWithGlow.replace(
-          /\[CHROME_STYLES\]/g,
-          chromeStyles
-        );
-        const finalTheme = themeWithChrome.replace(
-          /\[NEON_BRIGHTNESS\]/g,
-          neonBrightness
-        );
+        const chromeStyles = fs.readFileSync(__dirname +'/css/editor_chrome.css', 'utf-8');
+        const jsTemplate = fs.readFileSync(__dirname +'/js/theme_template.js', 'utf-8');
+        const themeWithGlow = jsTemplate.replace(/\[DISABLE_GLOW\]/g, disableGlow);
+        const themeWithChrome = themeWithGlow.replace(/\[CHROME_STYLES\]/g, chromeStyles);
+        const finalTheme = themeWithChrome.replace(/\[NEON_BRIGHTNESS\]/g, neonBrightness);
         fs.writeFileSync(templateFile, finalTheme, "utf-8");
-
+        
         // modify workbench html
         const html = fs.readFileSync(htmlFile, "utf-8");
 
@@ -93,58 +78,38 @@ function activate(context) {
 
         if (!isEnabled) {
           // delete cyber-synthwave script tag if there
-          let output = html.replace(
-            /^.*(<!-- CYBER 77 --><script src="neondreams.js"><\/script><!-- NEON DREAMS -->).*\n?/gm,
-            ""
-          );
+          let output = html.replace(/^.*(<!-- CYBER 77 --><script src="neondreams.js"><\/script><!-- NEON DREAMS -->).*\n?/mg, '');
           // add script tag
-          output = html.replace(
-            /\<\/html\>/g,
-            `	<!-- CYBER 77 --><script src="neondreams.js"></script><!-- NEON DREAMS -->\n`
-          );
-          output += "</html>";
-
+          output = html.replace(/\<\/html\>/g, `	<!-- CYBER 77 --><script src="neondreams.js"></script><!-- NEON DREAMS -->\n`);
+          output += '</html>';
+    
           fs.writeFileSync(htmlFile, output, "utf-8");
-
+          
           vscode.window
-            .showInformationMessage(
-              "Neon Dreams enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.",
-              { title: "Restart editor to complete" }
-            )
-            .then(function (msg) {
+            .showInformationMessage("Neon Dreams enabled. VS code must reload for this change to take effect. Code may display a warning that it is corrupted, this is normal. You can dismiss this message by choosing 'Don't show this again' on the notification.", { title: "Restart editor to complete" })
+            .then(function(msg) {
               vscode.commands.executeCommand("workbench.action.reloadWindow");
             });
-        } else {
-          vscode.window
-            .showInformationMessage(
-              "Neon dreams is already enabled. Reload to refresh JS settings.",
-              { title: "Restart editor to refresh settings" }
-            )
-            .then(function (msg) {
-              vscode.commands.executeCommand("workbench.action.reloadWindow");
-            });
-        }
-      } catch (e) {
-        if (/ENOENT|EACCES|EPERM/.test(e.code)) {
-          vscode.window.showInformationMessage(
-            "You must run VS code with admin priviliges in order to enable Neon Dreams."
-          );
-          return;
-        } else {
-          vscode.window.showErrorMessage(
-            "Something went wrong when starting neon dreams"
-          );
-          return;
-        }
-      }
-    }
-  );
+			} else {
+				vscode.window
+					.showInformationMessage('Neon dreams is already enabled. Reload to refresh JS settings.', { title: "Restart editor to refresh settings" })
+					.then(function(msg) {
+						vscode.commands.executeCommand("workbench.action.reloadWindow");
+					});
+			}
+		} catch (e) {
+			if (/ENOENT|EACCES|EPERM/.test(e.code)) {
+				vscode.window.showInformationMessage("You must run VS code with admin priviliges in order to enable Neon Dreams.");
+				return;
+			} else {
+				vscode.window.showErrorMessage('Something went wrong when starting neon dreams');
+				return;
+			}
+		}
+	});
 
-	let disable = vscode.commands.registerCommand(
-    "cyber77.disableNeon",
-    uninstall
-  );
-
+	let disable = vscode.commands.registerCommand('cyber77.disableNeon', uninstall);
+	
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disable);
 }
